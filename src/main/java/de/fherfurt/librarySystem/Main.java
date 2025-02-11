6ef6faffb65db1ba70b01e1cecc098ccab1bf44e
Stephanie <103069877+gymStephanie@users.noreply.github.com>
2025-02-11 21:18:34 +0100

    book class

--- src/main/java/de/fherfurt/librarySystem/Main.java
+++ src/main/java/de/fherfurt/librarySystem/Main.java
@@ -1,6 +1,13 @@
 package de.fherfurt.librarySystem;
 
+import de.fherfurt.librarySystem.models.Book;
+
 public class Main {
     public static void main(String[] args) {
+        Book book1 = new Book("Titel des Buchs", "Xander", "Komödie", false);
+        Book book2 = new Book("Titel des Buchssss", "Zeranski", "Horror", true);
+
+        System.out.println(book1);
+        System.out.println(book2);
     }
 }
