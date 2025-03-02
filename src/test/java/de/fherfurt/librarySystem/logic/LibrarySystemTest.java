33af9a28eaaa228de1e438eff8e544468e7d447c
Stephanie <103069877+gymStephanie@users.noreply.github.com>
2025-03-02 11:13:17 +0100

    init LibrarySystemTest

--- src/test/java/de/fherfurt/librarySystem/logic/LibrarySystemTest.java
+++ src/test/java/de/fherfurt/librarySystem/logic/LibrarySystemTest.java
@@ -1,7 +1,133 @@
 package de.fherfurt.librarySystem.logic;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibrarySystemTest {
+    private LibrarySystem librarySystem;
 
+    @BeforeEach
+    void setUp() {
+        // TODO
+        librarySystem = new LibrarySystem();
+    }
+
+    @AfterEach
+    void tearDown() {
+        // TODO
+        librarySystem = null;
+    }
+
+    @Test
+    void testAddBook() {
+        // TODO Lucas
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    void testEditBook() {
+        // TODO Lucas
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    void testDeleteBook() {
+        // TODO Lucas
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    void testAddPerson() {
+        // TODO Stephanie
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    void testEditPerson() {
+        // TODO Stephanie
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    void testDeletePerson() {
+        // TODO Stephanie
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    void testBorrowBook() {
+        // TODO Lucas
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    void testGaveBookBack() {
+        // TODO Lucas
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    void testFilterBooks() {
+        // TODO Josephina
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    void testFilterPersons() {
+        // TODO Stephanie
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    void testCalculateFeeForBook() {
+        // TODO Lucas
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
 }
