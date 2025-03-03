c08cc26056db45eac5525c18bf495b2fcb0f6d3c
PhinaB <josephina.burger@fh-erfurt.de>
2025-03-03 17:02:21 +0100

    BookFilter just to make the tests work, you can overwrite them

--- src/main/java/de/fherfurt/librarySystem/logic/BookFilter.java
+++ src/main/java/de/fherfurt/librarySystem/logic/BookFilter.java
@@ -1,4 +1,23 @@
 package de.fherfurt.librarySystem.logic;
 
+import de.fherfurt.librarySystem.models.Book;
+
 public class BookFilter {
+
+    public void setAuthor(String author) {
+        //TODO: Lucas
+    }
+    
+    public void setTitle(String title) {
+        //TODO: Lucas
+    }
+
+    public void setGenre(String genre) {
+        //TODO: Lucas
+    }
+
+    public boolean hasFilter() {
+        //TODO: Lucas
+        return false;
+    }
 }
