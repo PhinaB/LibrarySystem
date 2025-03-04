be38095acb06cfb0cf0c17ebaf577bf719d2029b
Stephanie <103069877+gymStephanie@users.noreply.github.com>
2025-03-04 20:26:17 +0100

    english main

--- src/main/java/de/fherfurt/librarySystem/Main.java
+++ src/main/java/de/fherfurt/librarySystem/Main.java
@@ -1,46 +1,20 @@
 package de.fherfurt.librarySystem;
 
 import de.fherfurt.librarySystem.logic.LibrarySystem;
-import de.fherfurt.librarySystem.logic.PersonFilter;
 import de.fherfurt.librarySystem.models.Book;
 import de.fherfurt.librarySystem.models.Person;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         LibrarySystem librarySystem = new LibrarySystem();
 
-        Book book1 = new Book("Titel des Buchs", "Xander", "Komödie", false);
-        Book book2 = new Book("Titel des Buchssss", "Zeranski", "Horror", true);
+        Book book1 = new Book("Title of book", "x", "comedy", false);
+        Book book2 = new Book("another title of book", "z", "horror", true);
 
-        Person person1 = new Person("Stephanie", "W", LocalDate.of(2000, 2, 2));
-
+        Person person1 = new Person("Hans", "W", LocalDate.of(2000, 2, 2));
         Person person2 = new Person("Marie", "K", LocalDate.of(2001,1,1));
 
-        person2.addNewBorrowedBook(book1);
-        person2.addNewBorrowedBook(book2);
-        librarySystem.addPerson(person1);
-        librarySystem.addPerson(person2);
-
-
-        System.out.println(person1.countBorrowedBooks());
-
-        PersonFilter personFilter = new PersonFilter();
-        personFilter.setMinBooksBorrowed(1);
-        personFilter.setMaxBooksBorrowed(3);
-        List<Person> filterResult = librarySystem.filterPersons(personFilter);
-
-        System.out.println(book1);
-        System.out.println(book2);
-        System.out.println(person1);
-        System.out.println("_________");
-        System.out.println(librarySystem.toString());
-        System.out.println("_________");
-        System.out.println(person2.toString());
-        System.out.println("_________");
-        System.out.println(filterResult);
     }
 }
